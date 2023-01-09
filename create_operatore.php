d66c3d324d2ed012412833f8aba579f087be43d6
StefanoMastrantonio <116554715+StefanoMastrantonio@users.noreply.github.com>
2023-01-09 16:37:53 +0100

    inserito phpmailer anche su creazione utente e creazione operatore

--- create_operatore.php
+++ create_operatore.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 require_once('config.php');
 if (isset($_SESSION['msg'])) {
     echo $_SESSION['msg'];
@@ -16,6 +23,43 @@ if (isset($_POST['name']) && (isset($_POST['email'])) && (isset($_POST['password
     $stmt->bindParam(':email', $_POST['email']);
     $stmt->bindParam(':password', $_POST['password']);
     $stmt->execute();
+
+    $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+//            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = '0870cc66cba737';                     //SMTP username
+            $mail->Password   = 'c8f42f7dc36756';                               //SMTP password
+//            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('from@example.com', 'Mailer');
+            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+            $mail->addAddress('ellen@example.com');               //Name is optional
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
+
+            //Attachments
+//            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'Nuovo operatore creato!<b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
 } else {
     header('location: form_creazione_operatore.php');
 }
